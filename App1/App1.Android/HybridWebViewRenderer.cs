f77398e52e9d7edf92f578cef7aafd6a7e0f5c0c
rspaimoc <raduspaimoc@gmail.com>
2019-05-16 08:02:01 +0200

    First

---
+++ App1/App1.Android/HybridWebViewRenderer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using App1.Droid;
+using App1;
+
+[assembly: ExportRenderer(typeof(HybridWebView), typeof(HybridWebViewRenderer))]
+namespace App1.Droid
+{
+    public class HybridWebViewRenderer : ViewRenderer<HybridWebView, Android.Webkit.WebView>
+    {
+        const string JavascriptFunction = "function invokeCSharpAction(data){jsBridge.invokeAction(data);}";
+        Context _context;
+
+        public HybridWebViewRenderer(Context context) : base(context)
+        {
+            _context = context;
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<HybridWebView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null)
+            {
+                var webView = new Android.Webkit.WebView(_context);
+                webView.Settings.JavaScriptEnabled = true;
+                webView.SetWebViewClient(new JavascriptWebViewClient($"javascript: {JavascriptFunction}"));
+                SetNativeControl(webView);
+            }
+            if (e.OldElement != null)
+            {
+                Control.RemoveJavascriptInterface("jsBridge");
+                var hybridWebView = e.OldElement as HybridWebView;
+                hybridWebView.Cleanup();
+            }
+            if (e.NewElement != null)
+            {
+                Control.AddJavascriptInterface(new JSBridge(this), "jsBridge");
+                Control.LoadUrl($"file:///android_asset/Content/{Element.Uri}");
+            }
+        }
+    }
+}
---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var hybridWebView = new HybridWebView
+            {
+                Uri = "index.html",
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+            //hybridWebView.RegisterAction(data => DisplayAlert("Alert", "Hello " + data, "OK"));
+            Padding = new Thickness(0, 20, 0, 0);
+            Content = hybridWebView;
+        }    
+    }
+}
